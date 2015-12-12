37a7ad60cb5f08b6f3dec9d157c301cde80b967c
Benjamin Wasty <benny.wasty@gmail.com>
2015-12-12 17:40:51 +0100

    WIP2 (35 compiler errors left)

--- Assets/Scripts/TreemapSquarify.js
+++ Assets/Scripts/TreemapSquarify.js
@@ -62,13 +62,13 @@ class Container {
 
     // getCoordinates - for a row of boxes which we've placed 
     //                  return an array of their cartesian coordinates
-    function getCoordinates(row : Array) {
-        var coordinates = [];
+    function getCoordinates(row : float[]) {
+        var coordinates = Array();
         var subxoffset = this.xoffset;
         var subyoffset = this.yoffset; //our offset within the container
         var areawidth = TreemapSquarify.sumArray(row) / this.height;
         var areaheight = TreemapSquarify.sumArray(row) / this.width;
-        var i;
+        var i : int;
 
         if (this.width >= this.height) {
             for (i = 0; i < row.length; i++) {
@@ -87,7 +87,7 @@ class Container {
     // cutArea - once we've placed some boxes into an row we then need to identify the remaining area, 
     //           this function takes the area of the boxes we've placed and calculates the location and
     //           dimensions of the remaining space and returns a container box defined by the remaining area
-    function cutArea(area) {
+    function cutArea(area : float) {
         var newcontainer;
 
         if (this.width >= this.height) {
@@ -105,72 +105,42 @@ class Container {
 }
 
 class Treemap {
+
+    // isArray - checks if arr is an array
+    var isArray = function(arr) {
+    	// TODO: internal compiler error...
+        //return arr && arr.constructor === Array; 
+    };
+
+    // sumMultidimensionalArray - sums the values in a nested array (aka [[0,1],[[2,3]]])
+    function sumMultidimensionalArray(arr : Array) : float {
+        var i : int;
+        var total = 0;
+
+        if(isArray(arr[0])) {
+            for(i=0; i<arr.length; i++) {
+                total += sumMultidimensionalArray(arr[i]);
+            }
+        } else {
+            total = TreemapSquarify.sumArray(arr);
+        }
+        return total;
+    };
+
 	function generate() {
         // normalize - the Bruls algorithm assumes we're passing in areas that nicely fit into our 
         //             container box, this method takes our raw data and normalizes the data values into  
         //             area values so that this assumption is valid.
-        var normalize = function (data, area) {
+        var normalize = function (data : float[], area : float) {
             var normalizeddata = [];
-            var sum = sumArray(data);
+            var sum = TreemapSquarify.sumArray(data);
             var multiplier = area / sum;
-            var i;
+            var i : int;
 
             for (i = 0; i < data.length; i++) {
                 normalizeddata[i] = data[i] * multiplier;
             }
             return normalizeddata;
-        };
-
-        // treemapMultidimensional - takes multidimensional data (aka [[23,11],[11,32]] - nested array)
-        //                           and recursively calls itself using treemapSingledimensional
-        //                           to create a patchwork of treemaps and merge them
-        var treemapMultidimensional = function(data, width, height, xoffset, yoffset) {
-            xoffset = (typeof xoffset === "undefined") ? 0 : xoffset;
-            yoffset = (typeof yoffset === "undefined") ? 0 : yoffset;
-            
-            var mergeddata = [];
-            var mergedtreemap;
-            var results = [];
-            var i;
-
-            if(isArray(data[0])) { // if we've got more dimensions of depth
-                for(i=0; i<data.length; i++) {
-                    mergeddata[i] = sumMultidimensionalArray(data[i]);
-                }
-                mergedtreemap = treemapSingledimensional(mergeddata, width, height, xoffset, yoffset);
-                
-                for(i=0; i<data.length; i++) {
-                    results.push(treemapMultidimensional(data[i], mergedtreemap[i][2] - mergedtreemap[i][0], mergedtreemap[i][3] - mergedtreemap[i][1], mergedtreemap[i][0], mergedtreemap[i][1]));
-                }
-            } else {
-                results = treemapSingledimensional(data,width,height, xoffset, yoffset);
-            };
-            return results;
-        };
-
-        // treemapSingledimensional - simple wrapper around squarify
-        var treemapSingledimensional = function(data, width, height, xoffset, yoffset) {
-            xoffset = (typeof xoffset === "undefined") ? 0 : xoffset;
-            yoffset = (typeof yoffset === "undefined") ? 0 : yoffset;
-
-            var rawtreemap = squarify(normalize(data, width * height), [], new Container(xoffset, yoffset, width, height), []);
-            return flattenTreemap(rawtreemap);
-        };
-
-        // flattenTreemap - squarify implementation returns an array of arrays of coordinates
-        //                  because we have a new array everytime we switch to building a new row
-        //                  this converts it into an array of coordinates.
-        var flattenTreemap = function(rawtreemap) {
-            var flattreemap = [];
-            var i;
-            var j;
-
-            for (i = 0; i < rawtreemap.length; i++) {
-                for (j = 0; j < rawtreemap[i].length; j++) {
-                    flattreemap.push(rawtreemap[i][j]);
-                }
-            }
-            return flattreemap;
         };
 
         // squarify  - as per the Bruls paper 
@@ -200,9 +170,72 @@ class Treemap {
             return stack;
         };
 
+        // treemapSingledimensional - simple wrapper around squarify
+        var treemapSingledimensional = function(data, width, height, xoffset, yoffset) {
+            xoffset = (typeof xoffset === "undefined") ? 0 : xoffset;
+            yoffset = (typeof yoffset === "undefined") ? 0 : yoffset;
+
+            var rawtreemap = squarify(normalize(data, width * height), [], new Container(xoffset, yoffset, width, height), []);
+            return flattenTreemap(rawtreemap);
+        };
+
+        // treemapMultidimensional - takes multidimensional data (aka [[23,11],[11,32]] - nested array)
+        //                           and recursively calls itself using treemapSingledimensional
+        //                           to create a patchwork of treemaps and merge them
+        var treemapMultidimensional = function(data : Array, width, height, xoffset, yoffset) {
+            xoffset = (typeof xoffset === "undefined") ? 0 : xoffset;
+            yoffset = (typeof yoffset === "undefined") ? 0 : yoffset;
+            
+            var mergeddata = [];
+            var mergedtreemap;
+            var results = [];
+            var i : int;
+
+            if(isArray(data[0])) { // if we've got more dimensions of depth
+                for(i=0; i<data.length; i++) {
+                    mergeddata[i] = sumMultidimensionalArray(data[i]);
+                }
+                mergedtreemap = treemapSingledimensional(mergeddata, width, height, xoffset, yoffset);
+                
+                for(i=0; i<data.length; i++) {
+                    results.push(treemapMultidimensional(data[i], mergedtreemap[i][2] - mergedtreemap[i][0], mergedtreemap[i][3] - mergedtreemap[i][1], mergedtreemap[i][0], mergedtreemap[i][1]));
+                }
+            } else {
+                results = treemapSingledimensional(data,width,height, xoffset, yoffset);
+            };
+            return results;
+        };
+
+        // flattenTreemap - squarify implementation returns an array of arrays of coordinates
+        //                  because we have a new array everytime we switch to building a new row
+        //                  this converts it into an array of coordinates.
+        var flattenTreemap = function(rawtreemap) {
+            var flattreemap = [];
+            var i;
+            var j;
+
+            for (i = 0; i < rawtreemap.length; i++) {
+                for (j = 0; j < rawtreemap[i].length; j++) {
+                    flattreemap.push(rawtreemap[i][j]);
+                }
+            }
+            return flattreemap;
+        };
+
+
+
+        // calculateRatio - calculates the maximum width to height ratio of the
+        //                  boxes in this row
+        var calculateRatio = function(row : float[], length) {
+            var min = Mathf.Min(row);
+            var max = Mathf.Max(row);
+            var sum = TreemapSquarify.sumArray(row);
+            return Mathf.Max(Mathf.Pow(length, 2) * max / Mathf.Pow(sum, 2), Mathf.Pow(sum, 2) / (Mathf.Pow(length, 2) * min));
+        };
+
         // improveRatio - implements the worse calculation and comparision as given in Bruls
         //                (note the error in the original paper; fixed here) 
-        var improvesRatio = function(currentrow, nextnode, length) {
+        var improvesRatio = function(currentrow : float[], nextnode, length) {
             var newrow; 
 
             if (currentrow.length === 0) {
@@ -220,34 +253,8 @@ class Treemap {
             return currentratio >= newratio; 
         };
 
-        // calculateRatio - calculates the maximum width to height ratio of the
-        //                  boxes in this row
-        var calculateRatio = function(row, length) {
-            var min = Math.min.apply(Math, row);
-            var max = Math.max.apply(Math, row);
-            var sum = sumArray(row);
-            return Math.max(Math.pow(length, 2) * max / Math.pow(sum, 2), Math.pow(sum, 2) / (Math.pow(length, 2) * min));
-        };
 
-        // isArray - checks if arr is an array
-        var isArray = function(arr) {
-            return arr && arr.constructor === Array; 
-        };
 
-        // sumMultidimensionalArray - sums the values in a nested array (aka [[0,1],[[2,3]]])
-        var sumMultidimensionalArray = function(arr) {
-            var i;
-            var total = 0;
-
-            if(isArray(arr[0])) {
-                for(i=0; i<arr.length; i++) {
-                    total += sumMultidimensionalArray(arr[i]);
-                }
-            } else {
-                total = sumArray(arr);
-            }
-            return total;
-        };
 
         return treemapMultidimensional; 
     }
