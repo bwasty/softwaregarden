55177d951abb8cfe7cbf581c776ab56e641892e2
Benjamin Wasty <benny.wasty@gmail.com>
2015-12-13 18:25:50 +0100

    added growing, another color

--- Assets/Scripts/CodeNode.cs
+++ Assets/Scripts/CodeNode.cs
@@ -3,11 +3,20 @@ using System.Collections;
 
 public class CodeNode : MonoBehaviour {
 
-	public static float startHeight = .25f;
-	public static float maxHeight = 4;
+	public float startHeight = .25f;
+	public float maxHeight = 4;
 
-	public static float thresholdYellow = 1;
-	public static float thresholdRed = 2;
+	public float thresholdGreen = 0.5f;
+	public float thresholdYellow = 1;
+	public float thresholdRed = 2;
+
+	public Color colorBlue = Color.blue;
+	public Color colorGreen = new Color32(17, 189, 61, 255);
+	public Color colorYellow = Color.yellow;
+	public Color colorRed = Color.red;
+	public Color colorDead = Color.black;
+
+	public float growthRate = 0.1f; // per second
 
 	private float height;
 	public float Height {
@@ -31,14 +40,16 @@ public class CodeNode : MonoBehaviour {
 
 	private void adaptColor() {
 		var mat = GetComponent<Renderer>().material;
-		if (height < thresholdYellow)
-			mat.color = Color.green;
+		if (height < thresholdGreen) 
+			mat.color = colorBlue;
+		else if (height < thresholdYellow)
+			mat.color = colorGreen;
 		else if (height < thresholdRed)
-			mat.color = Color.yellow;
+			mat.color = colorYellow;
 		else if (height < maxHeight)
-			mat.color = Color.red;
+			mat.color = colorRed;
 		else if (height >= maxHeight) 
-			mat.color = Color.black;
+			mat.color = colorDead;
 	}
 
 	// Use this for initialization
@@ -48,15 +59,17 @@ public class CodeNode : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Time.realtimeSinceStartup > 3) {
-			Height = 1;
-		}
-		if (Time.realtimeSinceStartup > 6) {
-			Height = 2;
-		}
-
-		if (Time.realtimeSinceStartup > 10) {
-			Height = 10;
-		}
+//		if (Time.realtimeSinceStartup > 2) {
+//			Height = 1;
+//		}
+//		if (Time.realtimeSinceStartup > 4) {
+//			Height = 2;
+//		}
+//
+//		if (Time.realtimeSinceStartup > 6) {
+//			Height = 10;
+//		}
+			
+		Height += growthRate * Time.deltaTime;
 	}
 }
