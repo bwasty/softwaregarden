c13b7c5954d04f011fd55255c05f8459ba5451bb
Benjamin Wasty <benny.wasty@gmail.com>
2015-12-14 15:57:59 +0100

    colors, game logic

--- Assets/Scripts/CodeNode.cs
+++ Assets/Scripts/CodeNode.cs
@@ -2,21 +2,27 @@
 using System.Collections;
 
 public class CodeNode : MonoBehaviour {
+	public GameManager manager;
 
-	public float startHeight = .25f;
+	public float startHeight = .15f;
 	public float maxHeight = 4;
 
 	public float thresholdGreen = 0.5f;
-	public float thresholdYellow = 1;
-	public float thresholdRed = 2;
+	public float thresholdYellow = 1.5f;
+	public float thresholdOrange = 2.5f;
+	public float thresholdRed = 3.75f;
 
 	public Color colorBlue = Color.blue;
 	public Color colorGreen = new Color32(17, 189, 61, 255);
 	public Color colorYellow = Color.yellow;
+	public Color colorOrange = new Color32(244, 178, 92, 155);
 	public Color colorRed = Color.red;
 	public Color colorDead = Color.black;
 
 	public float growthRate = 0.1f; // per second
+
+//	public float startArea = 100;
+//	public float Area {get; set;}
 
 	private float height;
 	public float Height {
@@ -38,38 +44,52 @@ public class CodeNode : MonoBehaviour {
 		}
 	}
 
+	// Use this for initialization
+	void Start () {
+		Height = startHeight;
+//		Area = startArea;
+	}
+
 	private void adaptColor() {
 		var mat = GetComponent<Renderer>().material;
 		if (height < thresholdGreen) 
 			mat.color = colorBlue;
 		else if (height < thresholdYellow)
 			mat.color = colorGreen;
-		else if (height < thresholdRed)
+		else if (height < thresholdOrange)
 			mat.color = colorYellow;
+		else if (height < thresholdRed)
+			mat.color = colorOrange;
 		else if (height < maxHeight)
 			mat.color = colorRed;
 		else if (height >= maxHeight) 
 			mat.color = colorDead;
 	}
 
-	// Use this for initialization
-	void Start () {
-		Height = startHeight;
+	void splitNode() {
+		var clone = Instantiate(gameObject);
+		var cloneNode = clone.GetComponent<CodeNode>();
+
+		var heightSplitPoint = Random.Range(.2f, .7f);
+
+		Height *= heightSplitPoint;
+		cloneNode.Height *= 1f - heightSplitPoint;
+
+		manager.addNode(cloneNode, this);
+		manager.layoutNodes();
+	}
+
+	void OnMouseDown() {
+		if (!manager.gameRunning || manager.nodes.Count > manager.maxNodes)
+			return;
+
+		if (height > thresholdYellow && height < maxHeight)
+			splitNode();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-//		if (Time.realtimeSinceStartup > 2) {
-//			Height = 1;
-//		}
-//		if (Time.realtimeSinceStartup > 4) {
-//			Height = 2;
-//		}
-//
-//		if (Time.realtimeSinceStartup > 6) {
-//			Height = 10;
-//		}
-			
-		Height += growthRate * Time.deltaTime;
+		if (manager.gameRunning)
+			Height += growthRate * Time.deltaTime;
 	}
 }
